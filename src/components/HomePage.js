28787c7f93c7f10eec52b32c9a3ab6ed5e53c4e8
Eric Laitman <elaitman1@gmail.com>
2019-05-16 13:09:26 -0400

    working split up for screen size

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,15 +12,9 @@ class HomePage extends React.Component{
   }
 
   componentDidMount = async ()=>{
-    // setTimeout(() =>
 
     await this.props.fetchArticles()
     this.setFeaturedArticles()
-
-
-
-
-  // }, 5000)}
   }
 
   handleClick = (event) =>{
@@ -32,7 +26,7 @@ class HomePage extends React.Component{
     this.props.history.push('/viewchosenarticle');
   }
 
-    handleHomePageArticles = () => {
+    handleHomePageArticlesSmall = () => {
 
       if (this.props.articles.length === 0){
         return <div>loading...</div>
@@ -40,7 +34,7 @@ class HomePage extends React.Component{
       let array = []
       let x = this.props.articles.length - 12
 
-      for(let i = this.props.articles.length - 4; i>= x; i-- ){
+      for(let i=this.props.articles.length - 8; i>= x; i-- ){
         array.push(<div id={this.props.articles[i].id} onClick={this.handleClick}>
           <div id='block'>
           <div id="title">{this.props.articles[i].title}</div>
@@ -54,6 +48,29 @@ class HomePage extends React.Component{
       return array
     }
 
+    handleHomePageArticlesMedium = () => {
+
+      if (this.props.articles.length === 0){
+        return <div>loading...</div>
+      }
+      let array = []
+      let x = this.props.articles.length - 12
+
+      for(let i=this.props.articles.length - 8; i>= x; i-- ){
+        array.push(<div id={this.props.articles[i].id} onClick={this.handleClick}>
+          <div id='block'>
+          <div id="title">{this.props.articles[i].title}</div>
+          <div id="typetag">{this.props.articles[i].tag}</div>
+          <div className="author">{this.props.articles[i].author}</div>
+          </div>
+          <img id="homepage" src={this.props.articles[i].image}/>
+          </div>
+        )
+      }
+      return array
+    }
+
+
     setFeaturedArticles = () => {
 
       if (this.props.articles.length === 0){
@@ -65,8 +82,33 @@ class HomePage extends React.Component{
         array.push(this.props.articles[i])
       }
       this.setState({featuredArticles: array})
+    }
+
+    setHomepageGridArticles = () => {
+      debugger
+      if (this.props.articles.length === 0){
+        return <div>loading...</div>
+      }
+      let array = []
+      let x = this.props.articles.length - 7
+      for(let i = this.props.articles.length - 4; i>= x; i-- ){
+            array.push(
+          <div id={this.props.articles[i].id} onClick={this.handleClick}>
+          <div id='block'>
+          <div id="title">{this.props.articles[i].title}</div>
+          <div id="typetag">{this.props.articles[i].tag}</div>
+          <div className="author">{this.props.articles[i].author}</div>
+          </div>
+          <img id="homepage" src={this.props.articles[i].image}/>
+          </div>
+        )
+      }
+      return array
 
     }
+
+
+
 
   render() {
 
@@ -83,7 +125,9 @@ class HomePage extends React.Component{
 
     return (
         <div>
-        {this.state.featuredArticles.length === 0? <div>loading...</div> :
+        {this.state.featuredArticles.length === 0?
+          <div>loading...</div>
+          :
         <Slider {...settings} className="hslider">
         <div id={this.state.featuredArticles[0].id} onClick={this.handleClick}>
          <div className='fblock'>
@@ -93,7 +137,6 @@ class HomePage extends React.Component{
          <img className="FeatureSlider" src={this.state.featuredArticles[0].image}/>
          </div>
          </div>
-
          <div id={this.state.featuredArticles[1].id} onClick={this.handleClick}>
           <div className='fblock'>
           <div className="ftitle">{this.state.featuredArticles[1].title}</div>
@@ -102,7 +145,6 @@ class HomePage extends React.Component{
           <img className="FeatureSlider" src={this.state.featuredArticles[1].image}/>
           </div>
           </div>
-
           <div id={this.state.featuredArticles[2].id} onClick={this.handleClick}>
            <div className='fblock'>
            <div className="ftitle">{this.state.featuredArticles[2].title}</div>
@@ -112,8 +154,13 @@ class HomePage extends React.Component{
            </div>
            </div>
         </Slider>
-      }
-      {this.handleHomePageArticles()}
+        }
+          <div>{this.props.size.medium?
+          [this.setHomepageGridArticles(),
+          this.handleHomePageArticlesMedium()]
+          :
+          this.handleHomePageArticlesSmall()
+        }</div>
       </div>
     )
   }
@@ -124,7 +171,8 @@ const mapStateToProps =
  (state, ownProps) => {
      return {
        articles: Object.values(state.articles),
-       vals: Object.keys(state.articles)
+       vals: Object.keys(state.articles),
+       size:state.browser.is
      };
    }
 
@@ -134,3 +182,162 @@ export default connect(
   mapStateToProps,
   { fetchArticles, chosenArticle }
 )(HomePage);
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import Slider from 'react-slick'
+// import './HomepageFeatureSlider.css'
+// import { connect } from 'react-redux';
+// import { fetchArticles, chosenArticle } from '../actions';
+//
+//
+// class HomePage extends React.Component{
+//
+//   state = {
+//     featuredArticles: []
+//   }
+//
+//   componentDidMount = async ()=>{
+//     // setTimeout(() =>
+//
+//     await this.props.fetchArticles()
+//     this.setFeaturedArticles()
+//
+//
+//
+//
+//   // }, 5000)}
+//   }
+//
+//   handleClick = (event) =>{
+//     let id = parseInt(event.currentTarget.id)
+//
+//     let foundArticle = this.props.articles.find(article=>article.id === id)
+//
+//     this.props.chosenArticle(foundArticle)
+//     this.props.history.push('/viewchosenarticle');
+//   }
+//
+//     handleHomePageArticles = () => {
+//
+//       if (this.props.articles.length === 0){
+//         return <div>loading...</div>
+//       }
+//       let array = []
+//       let x = this.props.articles.length - 12
+//       // for(let i = this.props.articles.length - 4; i>= x; i-- ){
+//       //   array.push(<div id={this.props.articles[i].id} onClick={this.handleClick}>
+//       //     <div id='block'>
+//       //     <div id="title">{this.props.articles[i].title}</div>
+//       //     <div id="typetag">{this.props.articles[i].tag}</div>
+//       //     <div className="author">{this.props.articles[i].author}</div>
+//       //     </div>
+//       //     <img id="homepage" src={this.props.articles[i].image}/>
+//       //     </div>
+//       //   )
+//       // }
+//
+//       for(let i = this.props.articles.length - 4; i>= x; i-- ){
+//         array.push(<div id={this.props.articles[i].id} onClick={this.handleClick}>
+//           <div id='block'>
+//           <div id="title">{this.props.articles[i].title}</div>
+//           <div id="typetag">{this.props.articles[i].tag}</div>
+//           <div className="author">{this.props.articles[i].author}</div>
+//           </div>
+//           <img id="homepage" src={this.props.articles[i].image}/>
+//           </div>
+//         )
+//       }
+//       return array
+//     }
+//
+//
+//     setFeaturedArticles = () => {
+//
+//       if (this.props.articles.length === 0){
+//         return <div>loading...</div>
+//       }
+//       let array = []
+//       let x = this.props.articles.length - 3
+//       for(let i = this.props.articles.length - 1; i>= x; i-- ){
+//         array.push(this.props.articles[i])
+//       }
+//       this.setState({featuredArticles: array})
+//
+//     }
+//
+//   render() {
+//
+//     let settings = {
+//         // dots: true,
+//         infinite: true,
+//         autoplay: true,
+//         useCss: true,
+//         autoplaySpeed: 3000,
+//         arrows: false,
+//         // slidesToShow: 1,
+//         // slidesToScroll: 1
+//       }
+//
+//     return (
+//         <div>
+//         {this.state.featuredArticles.length === 0? <div>loading...</div> :
+//         <Slider {...settings} className="hslider">
+//         <div id={this.state.featuredArticles[0].id} onClick={this.handleClick}>
+//          <div className='fblock'>
+//          <div className="ftitle">{this.state.featuredArticles[0].title}</div>
+//          <div className="ftypetag">{this.state.featuredArticles[0].tag}</div>
+//          <div className="fauthor">{this.state.featuredArticles[0].author}</div>
+//          <img className="FeatureSlider" src={this.state.featuredArticles[0].image}/>
+//          </div>
+//          </div>
+//
+//          <div id={this.state.featuredArticles[1].id} onClick={this.handleClick}>
+//           <div className='fblock'>
+//           <div className="ftitle">{this.state.featuredArticles[1].title}</div>
+//           <div className="ftypetag">{this.state.featuredArticles[1].tag}</div>
+//           <div className="fauthor">{this.state.featuredArticles[1].author}</div>
+//           <img className="FeatureSlider" src={this.state.featuredArticles[1].image}/>
+//           </div>
+//           </div>
+//
+//           <div id={this.state.featuredArticles[2].id} onClick={this.handleClick}>
+//            <div className='fblock'>
+//            <div className="ftitle">{this.state.featuredArticles[2].title}</div>
+//            <div className="ftypetag">{this.state.featuredArticles[2].tag}</div>
+//            <div className="fauthor">{this.state.featuredArticles[2].author}</div>
+//            <img className="FeatureSlider" src={this.state.featuredArticles[2].image}/>
+//            </div>
+//            </div>
+//         </Slider>
+//       }
+//       {this.handleHomePageArticles()}
+//       </div>
+//     )
+//   }
+// }
+//
+//
+// const mapStateToProps =
+//  (state, ownProps) => {
+//      return {
+//        articles: Object.values(state.articles),
+//        vals: Object.keys(state.articles)
+//      };
+//    }
+//
+//
+//
+// export default connect(
+//   mapStateToProps,
+//   { fetchArticles, chosenArticle }
+// )(HomePage);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,25 @@
+// import { combineReducers } from 'redux';
+// // import { reducer as formReducer } from 'redux-form';//here we rename as formreducer
+// // import authReducer from './authReducer';
+//
+// import articleReducer from './articleReducer';
+// import chosenArticle from './chosenArticle';
+// import searchInput from './searchInput';
+// import searchResults from './searchResults';
+// import clickedSection from './clickedSection';
+//
+//
+// export default combineReducers({
+//   searchResults: searchResults,
+//   searchInput: searchInput,
+//   articles: articleReducer,
+//   chosenArticle: chosenArticle,
+//   clickedSection: clickedSection
+// })
+
 import { combineReducers } from 'redux';
-// import { reducer as formReducer } from 'redux-form';//here we rename as formreducer
-// import authReducer from './authReducer';
+import {createResponsiveStateReducer} from 'redux-responsive'
+import {transform} from 'lodash'
 
 import articleReducer from './articleReducer';
 import chosenArticle from './chosenArticle';
@@ -14,5 +33,18 @@ export default combineReducers({
   searchInput: searchInput,
   articles: articleReducer,
   chosenArticle: chosenArticle,
-  clickedSection: clickedSection
+  clickedSection: clickedSection,
+  browser: createResponsiveStateReducer({
+        extraSmall: 380,
+        // small: 700,
+        medium: 769,
+        // large: 1280,
+        // extraLarge: 1400,
+        infinity: "veryBig",
+        extraFields: ({ greaterThan, is }) => ({
+          greaterThanOrEqual: transform(greaterThan, (result, value, mediaType) => {
+            result[mediaType] = value || is[mediaType]
+            }, {})
+        }),
+    }),
 })
