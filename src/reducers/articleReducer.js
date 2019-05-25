c678b582b46df56e4c055d40a71a9fe0bce06117
Eric Laitman <elaitman1@gmail.com>
2019-05-24 19:55:34 -0700

    edit

--- src/reducers/articleReducer.js
+++ src/reducers/articleReducer.js
@@ -1,60 +1,15 @@
 import _ from 'lodash';
 import {
   FETCH_ARTICLES,
-  // CHOSEN_ARTICLE,
   SEARCH_INPUT,
   SEARCH_RESULTS,
-  // FETCH_STREAMS,
-  // CREATE_STREAM,
-  // EDIT_STREAM,
-  // DELETE_STREAM
 } from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
-
     case FETCH_ARTICLES:
-    // return [{Object.assign(...state, {action.payload:id}))
       return action.payload;
-    // case CHOSEN_ARTICLE:
-    //
-    // let oldState = [...state]
-    // let newState = {chosenArticle: action.payload}
-    //   return [oldState, newState];
-
-      // case SEARCH_RESULTS:
-      //
-      //   return (
-      //     { searchResults: action.payload },
-      //     [...state]
-      //   );
-      //map keys will take an array and return an object... create new object. whatever id is use that as key and the value is everything else associated with the object. merges array of objects into one large object
-    // case FETCH_STREAM:
-    //   return { ...state, [action.payload.id]: action.payload };
-    // case CREATE_STREAM:
-    //   return { ...state, [action.payload.id]: action.payload };
-    // case EDIT_STREAM:
-    //   return { ...state, [action.payload.id]: action.payload };
-    // case DELETE_STREAM:
-    //   return _.omit(state, action.payload);
-    //   //lodash library
     default:
       return state;
   }
 };
-
-//array based approach
-// cont streamReducer = (state=[], action) => {
-// switch (action.type) {
-//   case EDIT_STREAM:
-//     return state.map(stream => {
-//       if (stream.id === action.payload.id) {
-//         return action.payload;
-//       }else{
-//         return stream;
-//       }
-//     })
-//     default:
-//     return state
-// }
-// }
